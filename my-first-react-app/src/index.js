2c2a26fdbd8c2479714bec96baae9a7f25575e0b
Paige Morton <paigeashley624@gmail.com>
2021-09-14 17:17:47 -0400

    add webpage used

--- my-first-react-app/src/index.js
+++ my-first-react-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// https://reactjs.org/tutorial/tutorial.html#inspecting-the-starter-code
 
 function Square(props) {
   return (
